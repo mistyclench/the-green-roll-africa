7401714e24759461146b5a5953fb76ec6b351ce4
mistyclench <mistyclench99@gmail.com>
2024-07-08 23:40:06 +0000

    homepage done

---
+++ app/components/testimonials.tsx
@@ -0,0 +1,38 @@
+export const testimonials = [
+    {
+  testimonial:
+    'In labore excepteur do exercitation do nisi tempor consequat ullamco ut et eiusmod. Nostrud anim esse dolore incididunt ipsum non veniam ut non duis. Adipisicing est cupidatat in pariatur. Consectetur eu aliqua voluptate cupidatat aute officia aute.',
+    client: 'client one',
+    image: '/Corporate group.jpg'
+},
+{
+    testimonial:
+      'Cillum magna adipisicing deserunt sit quis. Cupidatat laborum cillum reprehenderit reprehenderit cupidatat velit enim exercitation duis ex voluptate nulla. Ea tempor ipsum sit do enim dolor duis aliquip ullamco reprehenderit. Minim ipsum velit irure reprehenderit anim elit velit duis. Consequat elit occaecat cillum id id. Est non ea Lorem ullamco adipisicing dolore duis veniam tempor amet. Incididunt sit quis et do et.',
+      client: 'client two',
+      image: '/Corporate group.jpg'
+  },
+  {
+    testimonial:
+      'Magna magna occaecat tempor Lorem cupidatat aute aute. Velit esse amet reprehenderit non ea. Amet excepteur do elit pariatur. Sit qui nulla laboris aute velit velit ad esse. Enim dolor sunt magna eu laborum minim commodo ullamco ipsum dolor ipsum proident.',
+      client: 'client three',
+      image: '/Corporate group.jpg'
+  },    {
+    testimonial:
+      'In labore excepteur do exercitation do nisi tempor consequat ullamco ut et eiusmod. Nostrud anim esse dolore incididunt ipsum non veniam ut non duis. Adipisicing est cupidatat in pariatur. Consectetur eu aliqua voluptate cupidatat aute officia aute.',
+      client: 'client one',
+      image: '/Corporate group.jpg'
+  },
+  {
+      testimonial:
+        'Cillum magna adipisicing deserunt sit quis. Cupidatat laborum cillum reprehenderit reprehenderit cupidatat velit enim exercitation duis ex voluptate nulla. Ea tempor ipsum sit do enim dolor duis aliquip ullamco reprehenderit. Minim ipsum velit irure reprehenderit anim elit velit duis. Consequat elit occaecat cillum id id. Est non ea Lorem ullamco adipisicing dolore duis veniam tempor amet. Incididunt sit quis et do et.',
+        client: 'client two',
+        image: '/Corporate group.jpg'
+    },
+    {
+      testimonial:
+        'Magna magna occaecat tempor Lorem cupidatat aute aute. Velit esse amet reprehenderit non ea. Amet excepteur do elit pariatur. Sit qui nulla laboris aute velit velit ad esse. Enim dolor sunt magna eu laborum minim commodo ullamco ipsum dolor ipsum proident.',
+        client: 'client three',
+        image: '/Corporate group.jpg'
+    }
+  
+];
--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ import CallToActionCard from '../app/components/CallToActionCard';
 import truncateText from '../app/utils';
 import { articles, singleArticle } from '../app/utils/articles';
 import { Button } from './ui/button';
+import TestimonialCard from './ui/testimonial-card';
+import {testimonials} from './components/testimonials';
+
 
 export default function Page() {
   return (
@@ -48,10 +51,7 @@ export default function Page() {
         </div>
       </section>
 
-      <section
-        className="font-volkhov mt-60 h-[130px] text-black"
-        id="section-card"
-      >
+      <section className="section-card font-volkhov mt-60 h-[130px] text-black">
         <div className=" green border-[#707070]] min-w-8 mx-auto box-border h-20 w-8/12 rounded-[27px] border-[1px] border-solid">
           <div className="font-large row align-center objects-center flex flex-row content-center justify-center text-center">
             <div className="bg-darkcyan align-center objects-center h-[130px] content-center justify-center rounded-[17px] text-center" />
@@ -96,24 +96,51 @@ export default function Page() {
           Discover why
         </Button>
       </section>
-      <section className="testimonials"></section>
+      <section className="testimonials py-2 mx-24">
+        <h2 className="Reviews-header text-3xl text-white font-volkhov m-0 inline-block h-[65px] w-[315px] font-bold capitalize">
+          Reviews
+        </h2>
+        <div className='flex flex-row row'>
+        <p className="m-0 inline-block w-[816px] text-2xl text-white font-semibold">
+          You don&apos;t have to believe us, but(t) this is what others think of The Green Roll Africa:
+        </p>
+        <img
+          className="object-cover md:w-12 lg:w-24"
+          alt=""
+          src="/screenshot-20240618-091043removebgpreview@2x.png"
+        />
+        </div>
+        <div className='flex flex-row row overflow-x-scroll py-4 testimonials-scroll'>
+        {testimonials.map((testimonial) => {
+          return (
+        <TestimonialCard
+        testimonial={testimonial.testimonial}
+        client={testimonial.client}
+        image={testimonial.image}
+        />
+          );
+        })}
+
+        </div>
+
+      </section>
       <section
-        className="text-13xl font-poppins h-[806px] text-left text-black"
+        className="text-13xl font-poppins text-center text-black"
         id="mission"
       >
-        <div className="text-31xl text-orangered text-laft text-orange font-semibold">
-          Our mission: Let's make the world less shitty
+        <div className="text-2xl font-semibold text-[#FF5800]">
+          Our mission: Let&apos;s make the world less shitty
         </div>
-        <div className="inline-block h-48 font-semibold">
+        <div className="mx-12 my-2 inline-block w-auto text-left font-semibold">
           We are convinced that you can do a lot more with toilet paper than
           wiping your bum. All our products are made of
           environmentally-friendly, raw materials, and we donate 50% of our net
           profit to the building of sanitary facilities for those in need.
         </div>
-        <div className="text-darkorange font-semibold">
+        <div className="font-semibold text-[#FF8400]">
           We make serious impact
         </div>
-        <div className="bg-darkcyan green font-volkhov row mx-auto flex h-[70px] w-80 flex-row rounded-[17px] text-center text-gray-100">
+        <div className="bg-darkcyan green font-volkhov row mx-auto flex h-[70px] max-w-[500px] flex-row justify-between rounded-[17px] p-2 text-gray-100">
           <div className="row flex flex-row">
             <img
               className="h-[50px] w-[50px] rounded-[107px] bg-white p-2"
@@ -122,7 +149,9 @@ export default function Page() {
             />
             <div className="text-left">
               <p className="">42353</p>
-              <b className="font-segoe-ui text-orange=800">trees saved</b>
+              <p className="font-segoe-ui text-orange=800 h-[33px] w-[130px]">
+                trees saved
+              </p>
             </div>
           </div>
           <div className="row flex flex-row">
@@ -132,10 +161,10 @@ export default function Page() {
               src="/impacticon-toilets.svg"
             />
             <div className="text-left">
-              <b className="">233</b>
-              <b className="font-segoe-ui text-orangered inline-block h-[33px] w-[178px] text-left">
+              <p className="">233</p>
+              <p className="font-segoe-ui text-orangered inline-block h-[33px] w-[130px] text-left">
                 toilets built
-              </b>
+              </p>
             </div>
           </div>
           <div className="row flex flex-row">
@@ -146,24 +175,34 @@ export default function Page() {
             />
             <div className="text-left">
               <b className="">193150</b>
-              <b className="font-segoe-ui text-orangered inline-block h-[33px] w-[219px] text-left">
+              <b className="font-segoe-ui text-orangered inline-block h-[33px] w-[140px] text-left">
                 lives improved
               </b>
             </div>
           </div>
         </div>
 
-        <div className="font-segoe-ui border-dimgray row box-border flex h-[50px] max-h-[100px] w-[350px] w-auto max-w-[350px] flex-row justify-center align-center text-center objects-center m-auto rounded-[93px] border-[1px] border-solid bg-white my-2">
-          <div className="bg-khaki border-dimgray row flex h-full w-[350px] max-w-[500px] flex-row rounded-[98px] border-[1px] border-solid p-1">
-            <div className='py-1'>
-            <span>More about</span>
-            <b> our mission?</b>
+        <div className="font-segoe-ui border-dimgray row m-auto my-2 box-border flex h-[50px] max-h-[100px] w-[370px] max-w-[370px] flex-row rounded-[93px] border-[1px] border-solid bg-white">
+          <div className="bg-khaki border-dimgray row flex h-full w-[370px] max-w-[370px] flex-row rounded-[98px] border-[1px] border-solid p-1">
+            <div className="p-2">
+              <span className="font-light">More about</span>
+              <b> our mission?</b>
             </div>
-            <Button className="bg-[#FFDC63]">{`Read more >`}</Button>
+            <Button className="bg-[#FFDC63] hover:bg-yellow-400 focus-visible:outline-yellow-500 active:bg-yellow-600">{`Read more >`}</Button>
           </div>
         </div>
         <img className="float-right" alt="" src="/decore1.svg" />
         <img className="" alt="" src="/decore2.svg" />
+      </section>
+      <section
+        className=" row justified-center m-auto flex h-[136px] flex-row"
+        id="brands-logos"
+      >
+        <img className="min-w-[150px]" alt="brand-1" src="/G&W@2x.png" />
+        <img className="min-w-[150px]" alt="brand-2" src="/Jumbo@2x.png" />
+        <img className="min-w-[150px]" alt="brand-3" src="/eco-brand@2x.png" />
+        <img className="min-w-[150px]" src="/farm-fresh@2x.png" />
+        <img className="min-w-[150px]" alt="brand-5" src="/sync-ok@2x.png" />
       </section>
     </main>
   );
